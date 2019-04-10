54087cd3aa6fb7160e648ba83fe7a3c3142afb4b
julesy101 <alex@1n0.io>
2019-04-10 20:11:25 +0100

    api sdk tests

---
+++ sdk/chargersApi.js
@@ -0,0 +1,1 @@
+const modelValidator = require("../validation/validate");
---
+++ test/chargerApiTests.js
@@ -0,0 +1,24 @@
+
+describe("charger external sdk", () => {
+    it("rejects an invalid add charger model before calling the endpoint", () => {
+        
+    });
+    it("rejects an update charger model without and id populated", () => {
+
+    });
+    it("rejects an update model with invalid field types", () => {
+
+    });
+    it("no options passed creates an api proxy with process env APIBASEURL utilised", () => {
+
+    });
+    it("passing options creates an api with the base url set to the option passed", () => {
+
+    });
+    it("searchByCoordinates rejects non number lat, lng or radius", () => {
+
+    });
+    it("getCharger rejects empty string or null string as a search parameter", () => {
+
+    })
+});
