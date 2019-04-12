5ab187e0cfcd875709372dd3a4bd5c92843d5520
julesy101 <alex@1n0.io>
2019-04-12 06:58:25 +0100

    bolstered tests of sdk validation

--- test/chargerApiTests.js
+++ test/chargerApiTests.js
@@ -2,9 +2,7 @@ const sinon = require('sinon');
 const expect = require('chai').use(require('sinon-chai')).use(require('chai-as-promised')).expect;
 const ChargerApiSdk = require('../sdk/chargersApi');
 const RequestWrapper = require('../utilities/request-wrapper').RequestWrapper;
-const responseMocks = require('./mocks/ocmResponseMocks');
-const ocmMapper = require("../entities/charger").transformOcmEntity;
-
+const crp = require('./mocks/chargerRepositoryMocks');
 describe("charger external sdk", () => {
     it("rejects an invalid add charger model before calling the endpoint", async () => {
         let rputFake = sinon.stub(RequestWrapper.prototype, 'put').callsFake((url, body) => {});
@@ -20,31 +18,52 @@ describe("charger external sdk", () => {
         }
         expect(rputFake).to.not.be.called;
     });
-    it("add charger throws an error if model not valid", async () => {
+    it("add charger throws an error if model not valid", () => {
         let api = new ChargerApiSdk();
         expect(api.addCharger({
             notValidKey: "this isnt a valid key",
             notValidCharger: "this isnt a valid charger"
         })).to.be.rejectedWith(Error);
     });
-    it("rejects an update charger model without and id populated", () => {
+    it("rejects an update charger model without an id populated", () => {
         let api = new ChargerApiSdk();
-
-        expect(api.updateCharger())
+        expect(api.updateCharger({
+            notValidKey: "this isnt a valid key",
+            notValidCharger: "this isnt a valid charger"
+        })).to.be.rejectedWith(Error);
     });
     it("rejects an update model with invalid field types", () => {
+        let api = new ChargerApiSdk();
+        let dummyC = crp.allChargers()[0];
+        dummyC.network = "wait what?";
 
+        expect(api.updateCharger(dummyC)).to.be.rejectedWith(Error);
     });
     it("no options passed creates an api proxy with process env APIBASEURL utilised", () => {
-
-    });
+        let api = new ChargerApiSdk();
+        expect(api.baseUrl).to.be.equal("http://baseUrl.com/dev")
+    }); 
     it("passing options creates an api with the base url set to the option passed", () => {
-
+        let api = new ChargerApiSdk("https://testapi.com/dev");
+        expect(api.baseUrl).to.be.equal("https://testapi.com/dev");
     });
     it("searchByCoordinates rejects non number lat, lng or radius", () => {
-
+        let api = new ChargerApiSdk();
+        expect(api.searchChargerByCoordinates("not lat", -0.343, 10)).to.be.rejectedWith(Error);
+        expect(api.searchChargerByCoordinates(57.675, "not lng", 10)).to.be.rejectedWith(Error);
+        expect(api.searchChargerByCoordinates(57.675, -0.343, "not rad")).to.be.rejectedWith(Error);
     });
-    it("getCharger rejects empty string or null string as a search parameter", () => {
+    it("getCharger rejects empty string or null string as  parameter", () => {
+        let api = new ChargerApiSdk();
+        expect(api.getCharger("")).to.be.rejectedWith(Error);
+        expect(api.getCharger(null)).to.be.rejectedWith(Error);
+    })
 
+    beforeEach(() => {
+        process.env.APIBASEURL = "http://baseUrl.com/dev"
+    })
+
+    afterEach(() => {
+        delete process.env.APIBASEURL;
     })
 });
--- test/mocks/chargerRepositoryMocks.js
+++ test/mocks/chargerRepositoryMocks.js
@@ -3,6 +3,19 @@ const Charger = require("../../entities/charger")
 const ocmMapper = require("../../entities/charger").transformOcmEntity;
 
 class MockRepositoryResults {
+    allChargers(){
+        let chargers = JSON.parse(responseMocks.standardResponse).map(z => ocmMapper(z));
+        let final = [];
+        chargers.forEach((x, idx) => {
+            let charger = new Charger(x);
+            charger.id = "charger-id-" + idx;
+            charger.created = new Date().getTime();
+            charger.updated = new Date().getTime();
+            final.push(charger);
+        })
+        return final;
+    }
+
     ocmChargers(idsToInclude) {
         let finals = [];
         let chargers = JSON.parse(responseMocks.standardResponse).map(z => ocmMapper(z));
